6e6982cc59b10a7500a973fdb5686be43159dd6f
Rodrigo Mota <rodrigo.mota@base.digital>
2022-05-13 16:01:05 -0300

    feat(Crypto): add new methods

--- src/block.js
+++ src/block.js
@@ -1,4 +1,4 @@
-const SHA256 = require('crypto-js/sha256')
+const Crypto = require('./utils/crypto')
 
 const MINE_RATE = 3000 // 3 seconds
 
@@ -42,7 +42,7 @@ class Block {
   }
 
   static hash(timestamp, lastHash, data, nonce, difficulty) {
-    return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString()
+    return Crypto.hash(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString()
   }
 
   static hashFromBlock(block) {
--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256')
+
 const uuidv1 = require('uuid').v1 // unique id based on timestamp
 const EC = require('elliptic').ec // Elliptic Curve Cryptography
 const ec = new EC('secp256k1')
@@ -9,6 +11,14 @@ class Crypto {
 
   static genId() {
     return uuidv1()
+  }
+
+  static hash(data) {
+    return SHA256(JSON.stringify(data))
+  }
+
+  static verifySignature(address, signature, hash) {
+    return ec.keyFromPublic(address, 'hex').verify(hash, signature)
   }
 }
 
