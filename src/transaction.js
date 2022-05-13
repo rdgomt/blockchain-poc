f83182dae892b4efaede9b372c6f4c652ff6a4ed
Rodrigo Mota <rodrigo.mota@base.digital>
2022-05-13 16:02:00 -0300

    feat(Transaction): add new features

--- src/transaction.js
+++ src/transaction.js
@@ -8,17 +8,21 @@ class Transaction {
     this.outputs = []
   }
 
-  static create(senderWallet, recipientAddress, amount) {
-    if (amount > senderWallet.balance) {
+  verifyBalance(amount, balance) {
+    if (amount > balance) {
       throw new Error('Transaction Error: insufficient balance.')
     }
+  }
 
+  static create(senderWallet, recipientAddress, amount) {
     const transaction = new Transaction()
+
+    transaction.verifyBalance(amount, senderWallet.balance)
 
     transaction.outputs.push(...[
       // Debit transaction from sender's account
       {
-        amount: senderWallet.balance - amount,
+        amount: - amount,
         address: senderWallet.address,
       },
       // Credit transaction in the recipient's account
@@ -28,7 +32,44 @@ class Transaction {
       }
     ])
 
+    transaction.sign(senderWallet)
+
     return transaction
+  }
+
+  sign(senderWallet) {
+    const hash = Crypto.hash(JSON.stringify(this.outputs)).toString()
+
+    this.input = {
+      timestamp: Date.now(),
+      balance: senderWallet.balance,
+      address: senderWallet.address,
+      signature: senderWallet.sign(hash)
+    }
+  }
+
+  static verifySignature(transaction) {
+    const { address, signature } = transaction.input
+    const hash = Crypto.hash(JSON.stringify(transaction.outputs)).toString()
+
+    return Crypto.verifySignature(address, signature, hash)
+  }
+
+  update(senderWallet, recipientAddress, amount) {
+    const senderOutput = this.outputs.find((output) => output.address === senderWallet.address)
+
+    this.verifyBalance(amount, senderWallet.balance - senderOutput.amount)
+
+    senderOutput.amount = senderOutput.amount - amount
+
+    this.outputs.push({
+      amount,
+      address: recipientAddress,
+    })
+
+    this.sign(senderWallet)
+
+    return this
   }
 }
 
--- src/transaction.test.js
+++ src/transaction.test.js
@@ -18,13 +18,42 @@ describe('Transaction', () => {
     }).toThrowError()
   })
 
-  it("should register in the transaction's outputs the deduction from the sender's wallet", () => {
+  it("should register in it's outputs the deduction from the sender's wallet", () => {
     expect(transaction.outputs.find(output => output.address === senderWallet.address).amount)
-      .toEqual(senderWallet.balance - amount)
+      .toEqual(- amount)
   })
 
-  it("should register in the transaction's outputs the addition in the recipient's wallet", () => {
+  it("should register in it's outputs the addition in the recipient's wallet", () => {
     expect(transaction.outputs.find(output => output.address === recipientAddress).amount)
       .toEqual(amount)
+  })
+
+  it("should register in it's input the balance from sender's wallet", () => {
+    expect(transaction.input.balance).toEqual(senderWallet.balance)
+  })
+
+  it("should validate a valid transaction", () => {
+    expect(Transaction.verifySignature(transaction)).toBe(true)
+  })
+
+  it("should invalidate a corrupted transaction", () => {
+    transaction.outputs[0].amount = 50000
+
+    expect(Transaction.verifySignature(transaction)).toBe(false)
+  })
+
+  it("should be able to update a transaction (add new outputs)", () => {
+    const nextRecipientAddress = '0x8C70933F9EE2714B803940BA8ef0D2175f70bd66'
+    const nextAmount = 20
+    
+    transaction = transaction.update(senderWallet, nextRecipientAddress, nextAmount)
+
+    const senderOutput = transaction.outputs.find(output => output.address === senderWallet.address)
+    expect(senderOutput.amount).toEqual(- amount - nextAmount)
+
+    const nextRecipientOutput = transaction.outputs.find(output => output.address === nextRecipientAddress)  
+    expect(nextRecipientOutput.amount).toEqual(nextAmount)
+
+    expect(Transaction.verifySignature(transaction)).toBe(true)
   })
 })
